40368eb53051a153222509d3e275d51e9b63b2c8
Wajahat Riaz <61377755+WajahatRiaz@users.noreply.github.com>
2022-04-19 02:53:56 +0500

    Driver updated
    Driver updated with respect to generator

--- Sim/driver.sv
+++ Sim/driver.sv
@@ -1,42 +1,18 @@
+
 class driver;
 	transaction tr;
 	mailbox #(transaction) m1;
-
-	//mailbox handler
+	
+	//mailbox and virtual interface construct
 	function new(mailbox #(transaction) m1);
 		this.m1=m1;	
 	endfunction
 	
-	task get_INCR4();
-		for (int i=0;i<4;i++) begin
-		tr=new;
+	task run ();
+		forever begin 
+		tr=new();
 		m1.get(tr);
-		$display("Address:0x%0h recieved",tr.HADDR);
-		end	
-	endtask
-	
-	task get_INCR8();
-		for (int i=0;i<8;i++) begin
-		tr=new;
-		m1.get(tr);
-		$display("Address:0x%0h recieved",tr.HADDR);
-		end	
-	endtask
-
-	task get_INCR16();
-		for (int i=0;i<16;i++) begin
-		tr=new;
-		m1.get(tr);
-		$display("Address:0x%0h recieved",tr.HADDR);
-		end	
-	endtask
-
-
-	task get_SINGLE_BURST(integer beats);
-		for (int i=0;i<beats;i++) begin
-		tr=new;
-		m1.get(tr);
-		$display("Address:0x%0h recieved",tr.HADDR);
+		$display("Address:%0h Data:%0h",tr.HADDR,tr.HWDATA);
 		end	
 	endtask
 
