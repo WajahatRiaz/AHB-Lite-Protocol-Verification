670a2bc05b35195791ba7241fe31c3ba527e0dd5
Wajahat Riaz <61377755+WajahatRiaz@users.noreply.github.com>
2022-04-18 22:51:53 +0500

    Update interface.sv

--- Sim/interface.sv
+++ Sim/interface.sv
@@ -2,18 +2,15 @@ interface intf(input logic HCLK, HRESTn);
 	
 	
 	logic   [31:0] HWDATA; 
- 	logic   [31:0] HADDR; 	
-	
+ 	logic   [31:0] HADDR;
+	logic	[31:0] HRDATA;  
 	logic   [2:0]  HSIZE;	  
 	logic   [2:0]  HBURST;  
+	logic   [3:0]  HPROT;  
 	logic   [1:0]  HTRANS;  
-    	logic   [3:0]  HPROT;  
-	bit            HWRITE;  
-	logic	[31:0] HRDATA;   
+	logic          HWRITE;   
 	logic 	       HRESP; 
 	logic	       HREADY;
-
-modport driver();
 
 
 endinterface
