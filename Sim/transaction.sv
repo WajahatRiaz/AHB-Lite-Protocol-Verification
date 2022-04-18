6852e668740cb3bbc665fdb31ab0afc9c3579e90
Wajahat Riaz <61377755+WajahatRiaz@users.noreply.github.com>
2022-04-19 01:49:18 +0500

    Constraints added

--- Sim/transaction.sv
+++ Sim/transaction.sv
@@ -1,3 +1,4 @@
+
 class transaction;
 
 	//input signals to DUT
@@ -5,12 +6,23 @@ class transaction;
  	rand bit   [31:0] HADDR; 	
 	rand bit   [2:0]  HSIZE;	  
 	rand bit   [2:0]  HBURST;  
-	rand logic [1:0]  HTRANS;  
-    	rand logic [3:0]  HPROT;  
-	rand bit          HWRITE;  
+	rand bit   [1:0]  HTRANS;  
+    	rand bit   [3:0]  HPROT;  
+	rand bit          HWRITE; 
+	rand bit 	  HSEL; 
    
 	//Output signals from DUT
-	logic	   [31:0] HRDATA;   
+	bit	   [31:0] HRDATA;   
 	bit 	   	  HRESP; 
 	bit	          HREADY;
+
+ 	constraint constraints_c1 {
+   
+	 HSIZE  inside {3'b001, 3'b010 , 3'b000}; //HSIZE must be less than or equal to data bus
+	 HTRANS dist   {2'b00:=25,2'b01:=25,2'b10:=10,2'b11:=40}; //IDLE ,BUSY, NONSEQ, SEQ
+	 HPROT  inside {3'b001}; // protections signal for data fetch only
+	 HWRITE dist   {1'b1:=1,1'b0:=1};
+	 HSEL   inside {1'b1};	 // Because we have one slave
+ 	 }
+
 endclass: transaction
