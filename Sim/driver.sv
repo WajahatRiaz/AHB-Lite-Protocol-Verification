7181307c4337c15846686474a43dd15426072265
Wajahat Riaz <61377755+WajahatRiaz@users.noreply.github.com>
2022-04-19 05:38:07 +0500

    Update driver.sv

--- Sim/driver.sv
+++ Sim/driver.sv
@@ -1,11 +1,14 @@
 
+
 class driver;
 	transaction tr;
 	mailbox #(transaction) m1;
-	
+	virtual intf vif;
+
 	//mailbox and virtual interface construct
 	function new(mailbox #(transaction) m1);
-		this.m1=m1;	
+		this.m1=m1;
+	
 	endfunction
 	
 	task run ();
@@ -15,5 +18,25 @@ class driver;
 		$display("Address:%0h Data:%0h",tr.HADDR,tr.HWDATA);
 		end	
 	endtask
+
+	task reset(bit HRESTn);
+		if(!HRESTn); //HRESTn is the only active low signal
+  	  	vif.HADDR   <= 32'b0;
+	       	vif.HWDATA  <= 32'b0;
+		vif.HRDATA  <= 32'b0;
+ 	      	vif.HSIZE   <= 3'b010; // Word
+       		vif.HBURST  <= 3'b000; // Single Burst
+     		vif.HTRANS  <= 2'b00;  // IDLE
+     		vif.HWRITE  <= 1'b0;
+   	  	vif.HPROT   <= 4'b0;
+     		vif.HSEL    <= 1'b0;
+		vif.HRESP   <= 1'b0;
+		vif.HREADY  <= 1'b1;
+		vif.HREADYOUT <=1'b1;
+     		$display("Bus System Reset\n");
+       	 	
+	endtask 
+
+
 
 endclass: driver
