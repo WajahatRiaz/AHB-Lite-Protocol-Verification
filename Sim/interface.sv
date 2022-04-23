7642bfd37bc6d118235b5ba291f14a139242f9f0
Wajahat Riaz <61377755+WajahatRiaz@users.noreply.github.com>
2022-04-24 00:15:31 +0500

    Interface updated

--- Sim/interface.sv
+++ Sim/interface.sv
@@ -1,4 +1,4 @@
-interface intf(input logic HCLK, HRESTn);
+interface intf(input logic HCLK, HRESETn);
 	
 	logic   [31:0] HWDATA; 
  	logic   [31:0] HADDR;
@@ -13,5 +13,53 @@ interface intf(input logic HCLK, HRESTn);
 	logic	       HREADY;
 	logic 	       HREADYOUT;
 
+ modport driver_port(
+      	input 		HCLK,
+      	input  		HRESETn,
+      	output 		HSEL,HADDR,HWDATA,HWRITE,HRDATA,HSIZE,HBURST,HPROT,HTRANS,HREADY,
+	input 		HREADYOUT,HRESP
+  );
+
+clocking driver_cb @(posedge HCLK);
+
+	default input #1 output #1; //input and output skews
+	output 		HSEL,HADDR,HWDATA,HWRITE,HSIZE,HBURST,HPROT,HTRANS,HREADY;
+	input 		HREADYOUT,HRESP;
+
+endclocking 
+
+ modport monitor_port(
+	input 		HCLK,
+      	input  		HRESETn,
+      	input   	HSEL,
+  	input    	HADDR,
+ 	input    	HWDATA,
+  	input           HWRITE,
+  	input        	HSIZE,
+  	input        	HBURST,
+  	input        	HPROT,
+  	input        	HTRANS,
+  	input           HREADY,
+	output 		HREADYOUT,
+	output          HRESP,
+	output 		HRDATA
+  );
+
+modport dut(
+      	input  		HRESETn,
+      	input  		HCLK,
+      	input   	HSEL,
+  	input   	HADDR,
+ 	input   	HWDATA,
+  	output 		HRDATA,
+  	input           HWRITE,
+  	input       	HSIZE,
+  	input       	HBURST,
+  	input       	HPROT,
+  	input       	HTRANS,
+  	output 		HREADYOUT,
+  	input           HREADY,
+  	output          HRESP
+  );
 
 endinterface
