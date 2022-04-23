0c1f7ebe2f7b10a6309c90a225efaac06d584517
Wajahat Riaz <61377755+WajahatRiaz@users.noreply.github.com>
2022-04-24 00:11:10 +0500

    Driver updated

--- Sim/driver.sv
+++ Sim/driver.sv
@@ -1,42 +1,63 @@
-
-
+//`ifndef include_n
+//`include "transaction.sv"
+//`endif
 class driver;
 	transaction tr;
 	mailbox #(transaction) m1;
-	virtual intf vif;
-
-	//mailbox and virtual interface construct
-	function new(mailbox #(transaction) m1);
-		this.m1=m1;
+	virtual interface intf.driver_port vif;
 	
+	//mailbox and virtual interface construct
+	function new(mailbox #(transaction) m1,virtual interface intf.driver_port vif);
+$display("Entering in driver block");
+		this.m1=m1;
+		this.vif=vif;
 	endfunction
 	
-	task run ();
+
+
+	task drive();
+		//forever begin	
+		tr=new;
+		m1.get(tr);
+
+		$display("Bus System Sending signals\n");	
+		@(posedge vif.HCLK);		
+		vif.HSEL 	<= this.tr.HSEL ;
+		vif.HADDR	<= this.tr.HADDR;
+		vif.HWDATA	<= this.tr.HWDATA;
+		vif.HWRITE	<= this.tr.HWRITE;
+		vif.HSIZE	<= this.tr.HSIZE;
+		vif.HBURST 	<= this.tr.HBURST;
+		vif.HPROT	<= this.tr.HPROT;
+		vif.HTRANS	<= this.tr.HTRANS;
+		vif.HREADY	<= this.tr.HREADY;
+
+		//end
+	endtask
+     		
+
+
+	task reset();	
+			
+		$display("Bus System Reset\n");			
+		vif.HSEL 	<= 0;
+		vif.HADDR	<= 0;
+		vif.HWDATA	<= 0;
+		vif.HWRITE	<= 0;
+		vif.HSIZE	<= 0;
+		vif.HBURST 	<= 0;
+		vif.HPROT	<= 0;
+		vif.HTRANS	<= 0;
+		vif.HREADY	<= 0;
+endtask
+   	task run();
 		forever begin 
 		tr=new();
 		m1.get(tr);
 		$display("Address:%0h Data:%0h",tr.HADDR,tr.HWDATA);
-		end	
-	endtask
-
-	task reset(bit HRESTn);
-		if(!HRESTn); //HRESTn is the only active low signal
-  	  	vif.HADDR   <= 32'b0;
-	       	vif.HWDATA  <= 32'b0;
-		vif.HRDATA  <= 32'b0;
- 	      	vif.HSIZE   <= 3'b010; // Word
-       		vif.HBURST  <= 3'b000; // Single Burst
-     		vif.HTRANS  <= 2'b00;  // IDLE
-     		vif.HWRITE  <= 1'b0;
-   	  	vif.HPROT   <= 4'b0;
-     		vif.HSEL    <= 1'b0;
-		vif.HRESP   <= 1'b0;
-		vif.HREADY  <= 1'b1;
-		vif.HREADYOUT <=1'b1;
-     		$display("Bus System Reset\n");
-       	 	
-	endtask 
-
-
+		//drive();
+		end
+	endtask  		
+     
 
 endclass: driver
